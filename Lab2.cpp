c354c3bccb170ec2d24e230baa08a67106dff4d9
s32217 <s32217@GDA403-13>
2023-10-14 10:59:18 +0200

    Poprawiono blad w dodatkowym

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
+#include "Dodatkowe.h"
 
 using namespace std;
 
 int main()
 {
-    int ilosc_pizzy = 0;
-    int ilosc_uczestnikow = 0;
-
-    cout << "Ile kawalkow pizzy jest: ";
-    cin >> ilosc_pizzy;
-    cout << "Ile uczestnikow imprezy jest: ";
-    cin >> ilosc_uczestnikow;
-
-    int per_uczestnik = ilosc_pizzy / ilosc_uczestnikow;
-
-    cout << "Kazdy uczestnik dostanie: " << (per_uczestnik == 1) ? " kawalek pizzy\n" : " kawalkow pizzy\n";
-    cout << "Dla organizatora zostanie: " << (ilosc_pizzy - (per_uczestnik * ilosc_uczestnikow) == 1) ? " kawalek pizzy\n" : " kawalkow pizzy";
+	dodatkowe();
 }
